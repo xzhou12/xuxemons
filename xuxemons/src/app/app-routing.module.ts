337df89646714eb0d077829daa4588bd8dde0dd8
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2024-02-16 20:07:23 +0100

    16/2/2024
    Login y registro añadidos

--- xuxemons/src/app/app-routing.module.ts
+++ xuxemons/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'error', component: ErrorComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'error',
+    redirectTo: '/error',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/error',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
